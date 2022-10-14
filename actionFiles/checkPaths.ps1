63d9b7e402ec1bca5938bbc4b41da0d258066fac
Nico Egli <nico.egli@mcs.ch>
2022-10-14 15:37:15 +0200

    added checkpaths.ps1

---
+++ actionFiles/checkPaths.ps1
@@ -0,0 +1,17 @@
+﻿[String[]]$toc = Get-Content .\actionFiles\toc.txt
+
+$folder = "doc/"
+
+foreach($line in $toc){
+    [String]$trimmed = $line.Trim()
+
+    if($trimmed.Length -eq 0){
+        continue
+    }
+
+    if($trimmed.EndsWith(".md")){
+        if(!(Test-Path $folder$trimmed)){
+            throw "`"$trimmed`" not found"
+        }
+    }
+}
